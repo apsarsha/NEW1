d0a4f80768b963118f66931de15b4ea7a88841f0
shaik apsar <apsarsk823@gmail.com>
2022-08-16 22:08:50 +0530

    NEW-1

---
+++ phone.js
@@ -0,0 +1,34 @@
+const phoneImages = ["images/black.png", "images/blue.jpg", "images/pink.jpg", "images/starlight.jpg"]
+let phoneCont = document.getElementById("phoneShow")
+
+let black = document.getElementById("black")
+
+let blue = document.getElementById("blue")
+
+let pink = document.getElementById("pink")
+
+let starlight = document.getElementById("starlight")
+
+
+let defaultImgItems =`<img src= "${phoneImages.at(0)}">`
+
+phoneCont.innerHTML = defaultImgItems
+
+let blueImgItems = `<img src= "${phoneImages.at(1)}">`
+let pinkImgItems = `<img src= "${phoneImages.at(2)}">`
+let starImgItems = `<img src= "${phoneImages.at(3)}">`
+
+black.addEventListener("click", function(){
+    phoneCont.innerHTML=defaultImgItems
+    })
+blue.addEventListener("click", function () {
+    phoneCont.innerHTML = blueImgItems
+})
+
+pink.addEventListener("click", function () {
+    phoneCont.innerHTML = pinkImgItems
+})
+
+starlight.addEventListener("click", function () {
+    phoneCont.innerHTML = starImgItems
+})   
